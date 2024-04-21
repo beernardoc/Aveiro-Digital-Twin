57d76a7decc792ada1dafa52eb2f8f24e59ce043
Zé Mendes <mendes.j@ua.pt>
2024-04-22 00:17:41 +0100

    Added test to method add_vehicle

--- Adapters/test/history_test.py
+++ Adapters/test/history_test.py
@@ -27,3 +27,13 @@ class TestFileComposer(unittest.TestCase):
         self.assertEqual(self.file_composer.root[-1][0].attrib, {'edges': '-456 -654 -564'})
         # make sure that the route was added to the correct vehicle
         self.assertEqual(self.file_composer.root[-1].attrib['id'], vehicle_id)
+
+    def test_add_vehicle(self):
+        vehicle = {'id': '0', 'type': 'test_car', 'depart': '0'}
+        route = ['-123', '-321', '-231']
+        self.file_composer.add_vehicle(vehicle, route)
+
+        self.assertEqual(self.file_composer.root[1].tag, 'vehicle')
+        self.assertEqual(self.file_composer.root[1].attrib, vehicle)
+        self.assertEqual(self.file_composer.root[1][0].tag, 'route')
+        self.assertEqual(self.file_composer.root[1][0].attrib, {'edges': '-123 -321 -231'})
