060aaf76deb7741c16aa2dc13e168666178f8f44
Zé Mendes <mendes.j@ua.pt>
2024-04-22 00:17:20 +0100

    Added test to method add_route_tag

--- Adapters/test/history_test.py
+++ Adapters/test/history_test.py
@@ -13,3 +13,17 @@ class TestFileComposer(unittest.TestCase):
         self.assertEqual(self.file_composer.root[-1].attrib, vehicle)
         # inside the vehicle tag it should be empty
         self.assertEqual(len(self.file_composer.root[-1]), 0)
+
+    def test_add_route_tag(self):
+        # for a rouste to be added, the vehicle tag must be added first
+        vehicle = {'id': '0', 'type': 'car', 'depart': '0'}
+        self.file_composer.add_vehicle_tag(vehicle)
+
+        route = ['-456', '-654', '-564']
+        vehicle_id = '0'
+        self.file_composer.add_route_tag(route, vehicle_id)
+
+        self.assertEqual(self.file_composer.root[-1][0].tag, 'route')
+        self.assertEqual(self.file_composer.root[-1][0].attrib, {'edges': '-456 -654 -564'})
+        # make sure that the route was added to the correct vehicle
+        self.assertEqual(self.file_composer.root[-1].attrib['id'], vehicle_id)
