cfd1e017a618ab719e4d51ee6376f8c0e3c3bc10
Zé Mendes <mendes.j@ua.pt>
2024-05-01 22:15:25 +0100

    Added Roundabout blocks in the general blocks page using the Cards

--- react-electron/src/pages/Block.js
+++ react-electron/src/pages/Block.js
@@ -1,10 +1,36 @@
 import './Block.css';
+import socketIOClient from 'socket.io-client';
+import Card from '../components/BlockedCard';
+import React, { useState, useEffect } from 'react';
 
 export default function Block() {
+
+  const [message, setMessage] = useState('');
+  const [blockedRoundabouts, setBlockedRoundabouts] = useState(null);
 
   const blockRoundabout = () => {
     window.location.href = '/block-roundabout';
   }
+
+  useEffect(() => {
+    const socket = socketIOClient('http://localhost:5000'); 
+
+    socket.on('blocked_roundabouts', (data) => {
+        // Convert the ArrayBuffer message to a string
+        const decoder = new TextDecoder();
+        const decodedMessage = decoder.decode(data);
+        
+        setMessage(decodedMessage);
+
+        // Convert the JSON string to a JavaScript object
+        const parsed = JSON.parse(decodedMessage);
+        // it is a map, get the keys and convert to an array
+        const blocked_roundabouts = Object.keys(parsed.blocked_roundabouts)
+        setBlockedRoundabouts(blocked_roundabouts);
+    });
+
+    return () => socket.disconnect();
+}, []);
   
   return (
     <div className="block-page-container">
@@ -20,8 +46,13 @@ export default function Block() {
             <div className="block-page-blocks" style={{ marginTop: "30px" }}>
                 <div className="block-page-block">
                     <h3 style={{color: "black", fontSize: "20px", paddingBottom: "10px"}}>Active Blocks</h3>
-                    <div className="">
-                        <h4>BLOCK CARDS</h4>
+                    <div className="blocked-roundabouts">
+                        <h2 style={{ color: "black", fontSize: "30px", marginTop: "20px" }}>Blocked Roundabouts</h2>
+                        {blockedRoundabouts && blockedRoundabouts.map((roundabout, index) => (
+                            <div key={index} className="blocked-roundabout">
+                                <Card id={roundabout} type="roundabout" />
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
