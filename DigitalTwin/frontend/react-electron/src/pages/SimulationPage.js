114a1248523e11c3f7c2b75a899e8f4efb5c2b8b
obrist <filipe.obrist@ua.pt>
2024-05-26 01:39:27 +0100

    Frontend - Simulation Page done, but do be reviewed

--- DigitalTwin/frontend/react-electron/src/pages/SimulationPage.js
+++ DigitalTwin/frontend/react-electron/src/pages/SimulationPage.js
@@ -38,18 +38,24 @@ const SimulationPage = () => {
     <div className="simulation-page">
         {parsedMessage && (
             <>
-                <p>
-                    You can view the simulation running in different environments: 
+                <div className="instructions">
+                    <h2>You can view the simulation running in different environments: 
                     <strong> Sumo</strong> for 2D simulations and <strong>Carla</strong> for 3D simulations.
-                </p>
+                    </h2>
+                </div>
                 <div className="simulation-info-card">
                     <h1><b> Simulation Info</b></h1>
                     <br />
                     <p className='quantity-card'><h4><FontAwesomeIcon icon={faClock} /> Time (s)</h4> {parsedMessage.time}</p>
                     <p className='quantity-card'><h4><FontAwesomeIcon icon={faCar} /> Total Vehicles</h4> {parsedMessage.vehicle && parsedMessage.vehicle.quantity}</p>
-                    <p className='quantity-card'><h4>Simulated Vehicles</h4> {parsedMessage.vehicle && parsedMessage.vehicle.ids && parsedMessage.vehicle.ids.filter(id => id.startsWith('simulated')).length}</p>
-                    <p className='quantity-card'><h4>Real Data Vehicles</h4> {parsedMessage.vehicle && parsedMessage.vehicle.ids && parsedMessage.vehicle.ids.filter(id => id.startsWith('realdata')).length}</p>
-                    <p className='quantity-card'><h4>Live Data Vehicles</h4> {parsedMessage.vehicle && parsedMessage.vehicle.ids && parsedMessage.vehicle.ids.filter(id => id.startsWith('livedata')).length}</p>
+                    <p className="vehicles-type-card">
+                        <h4>Simulated Vehicles</h4> 
+                        {parsedMessage.vehicle && parsedMessage.vehicle.ids && 
+                            (parsedMessage.vehicle.ids.filter(id => id.startsWith('simulated')).length + 
+                            parsedMessage.vehicle.ids.filter(id => id.startsWith('random')).length)}
+                    </p>
+                    <p className='vehicles-type-card'><h4>Real Data Vehicles</h4> {parsedMessage.vehicle && parsedMessage.vehicle.ids && parsedMessage.vehicle.ids.filter(id => id.startsWith('realdata')).length}</p>
+                    <p className='vehicles-type-card'><h4>Live Data Vehicles</h4> {parsedMessage.vehicle && parsedMessage.vehicle.ids && parsedMessage.vehicle.ids.filter(id => id.startsWith('livedata')).length}</p>
                     <h4>Legend</h4>
                     <ul>
                         <li><span className="legend-color" style={{ backgroundColor: 'red' }}></span> Real data</li>
@@ -59,13 +65,12 @@ const SimulationPage = () => {
                 </div>
                 <div className="simulation-methods-info">
                     <h3>Simulation Methods</h3>
-                    <p>
-                        The simulation can use various methods to generate vehicle data:
-                    </p>
                     <ul>
-                        <li><b>Real Data:</b> This data comes from actual vehicles on the road, collected through sensors and other monitoring equipment.</li>
-                        <li><b>Live Data:</b> This data is captured in real-time from live traffic feeds and other dynamic sources.</li>
-                        <li><b>Simulated Data:</b> This data is generated through computational models that simulate traffic conditions based on predefined parameters and scenarios.</li>
+                        <li><b>Live Data:</b> This data comes from actual vehicles on the road, collected through sensors and other monitoring equipment.</li>
+                        <li><b>Real Data:</b> Similar to live data, but this method utilizes historical data collected from previous days or hours.</li>
+                        <li><b>Simulated Data:</b> The routes of the simulated cars can be either randomly generated or specifically defined.</li>
+                        <li><b>Road Block:</b> You can also experiment with traffic flow by blocking roads to observe the resulting traffic patterns.</li>
+                        <li><b>Clear Simulation:</b> Clears all vehicles of the simulation</li>
                     </ul>
                 </div>
             </>
