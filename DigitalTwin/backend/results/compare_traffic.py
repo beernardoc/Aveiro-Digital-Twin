605574dd8387dd88990da34933d9ae8fce6808c3
beernardoc <bernardopinto@ua.pt>
2024-06-02 14:36:42 +0100

    generation of graphics for roundabouts

--- DigitalTwin/backend/results/compare_traffic.py
+++ DigitalTwin/backend/results/compare_traffic.py
@@ -5,38 +5,102 @@ import matplotlib.pyplot as plt
 def cars_per_edge(file):
     with open(file, 'r') as f:
         data = json.load(f)
-    return data
 
-def generate_graph(file1, file2):
+    # Excluir chaves que começam com ":"
+    relevant_data = {key: value for key, value in data.items() if not key.startswith(':')}
+
+    return relevant_data
+
+def generate_RoundaboutGraph(file1, file2):
     data1 = cars_per_edge(file1)
     data2 = cars_per_edge(file2)
 
-    keys = sorted(list(set(data1.keys()).union(set(data2.keys()))))  # Converting to list and sorting
+    with open('../Adapters/co_simulation/roundabout.json', 'r') as f:
+        roundabout_data = json.load(f)
+
+    # Inicializar dicionários de resultados antes e depois
+    results_before = {roundabout['id']: 0 for roundabout in roundabout_data}
+    results_after = {roundabout['id']: 0 for roundabout in roundabout_data}
+
+    for roundabout in roundabout_data:
+        roundabout_edges = roundabout['edges']
+        for edge in roundabout_edges:
+            results_before[roundabout["id"]] += data1[str(edge)]
+            results_after[roundabout["id"]] += data2[str(edge)]
 
     difference = {}
-    for key in keys:
-        value1 = data1.get(key, 0)
-        value2 = data2.get(key, 0)
+    for key in roundabout_data:
+        value1 = results_before[key['id']]
+        value2 = results_after[key['id']]
+        difference[key['id']] = value2 - value1
+
+    # Removing keys with zero difference
+    keys = list(difference.keys())
+    values = list(difference.values())
+
+    colors = ['orange' if v > 0 else 'blue' for v in values]
+
+    # Ajuste a largura das barras
+    bar_width = 0.5  # Defina a largura da barra conforme necessário
+
+    plt.bar(keys, values, color=colors, width=bar_width)
+    plt.xlabel('Roundabout')
+    plt.ylabel('Difference in the Number of Cars')
+    plt.title('Difference in the Number of Cars per roundabout')
+
+    # Definindo os ticks do eixo x como os IDs das rotatórias
+    plt.xticks(keys)
+
+    plt.tight_layout()  # Prevent labels from overlapping
+
+    plt.savefig('RoundAboutTraffic_difference.png')
+    plt.close()
+
+
+
+
+def generate_RoadGraph(file1, file2):
+    data1 = cars_per_edge(file1)
+    data2 = cars_per_edge(file2)
+
+
+    with open('../Adapters/co_simulation/road.json', 'r') as f:
+        road_data = json.load(f)
+
+    results_before = {road: 0 for road in road_data}
+    results_after = {road: 0 for road in road_data}
+
+    for road in road_data:
+        road_edges = road_data[road]["edges"]
+        for edge in road_edges:
+            results_before[road] += data1[edge]
+            results_after[road] += data2[edge]
+
+
+    difference = {}
+    for key in road_data:
+        value1 = results_before[key]
+        value2 = results_after[key]
         difference[key] = value2 - value1
 
     # Removing keys with zero difference
     difference = {key: value for key, value in difference.items() if value != 0}
-
     keys = list(difference.keys())
     values = list(difference.values())
 
     colors = ['orange' if v > 0 else 'blue' for v in values]
 
     plt.bar(keys, values, color=colors)
-    plt.xlabel('Edges')
+    plt.xlabel('Road')
     plt.ylabel('Difference in the Number of Cars')
-    plt.title('Difference in the Number of Cars per Edge')
+    plt.title('Difference in the Number of Cars per road')
     plt.xticks(rotation='vertical')  # Putting edge names vertically
-    plt.tick_params(axis='x', labelsize=4)  # Reduce font size on x-axis labels
+    plt.tick_params(axis='x', labelsize=5)  # Reduce font size on x-axis labels
     plt.tight_layout()  # Prevent labels from overlapping
 
-    plt.savefig('traffic_difference.png')
+    plt.savefig('RoadTraffic_difference.png')
     plt.close()
+
 
 
 if __name__ == "__main__":
@@ -47,4 +111,5 @@ if __name__ == "__main__":
     file1 = sys.argv[1]
     file2 = sys.argv[2]
 
-    generate_graph(file1, file2)
+    generate_RoadGraph(file1, file2)
+    generate_RoundaboutGraph(file1, file2)
